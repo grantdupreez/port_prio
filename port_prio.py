6f5f84e146b10613181ad6d9e4d2c91fd24e44c0
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-03-17 07:29:00 +0000

    Create port_prio.py

---
+++ port_prio.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import ipywidgets as widgets
+import plotly.express as px
+import plotly.graph_objs as go
+
+A = st.number_input("Enter value A:")
+B = st.number_input("Enter value A:")
+C = st.number_input("Enter value A:")
+D = st.number_input("Enter value A:")
+E = st.number_input("Enter value A:")
